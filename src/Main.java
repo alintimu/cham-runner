61d2f048b8040417daf154849f675126402b0761
alin.timu <alin.timu@xoomworks.com>
2014-08-08 18:13:59 +0300

    A LOOOOOOOOOOOOT

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,39 @@
 import Controller.MainController;
+import Model.ProjectPathList;
+import Model.ProjectPathsModel;
 import Repository.AbstractRepository;
 import Repository.EmbeddedRepository;
+import Util.JaxbUtils;
 import View.AbstractWindowView;
 import View.MainWindowView;
+
+import javax.xml.bind.JAXBException;
 
 public class Main {
     private static final String MAIN_PAGE_VIEW = "mainView";
     private static final String EMBEDDED_REPOSITORY = "mainRepository";
 
     public static void main(String[] args) {
+        ProjectPathList pathList = new ProjectPathList();
+        pathList.getModelList().add(new ProjectPathsModel(1, "test/estes/fs/fsa/dsf/ds", "SS", true, ""));
+        pathList.getModelList().add(new ProjectPathsModel(2, "dsa/dsa/dsa", "API", false, ""));
+
+        try {
+            String xxx = JaxbUtils.convertToXml("aaaaa.xml", ProjectPathList.class, pathList);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            ProjectPathList pathList2 = (ProjectPathList) JaxbUtils.convertToObject("aaaaa.xml", ProjectPathList.class);
+
+            System.out.println(pathList2);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+
         AbstractWindowView mainView = new MainWindowView(MAIN_PAGE_VIEW);
         AbstractRepository mainRepository = new EmbeddedRepository(EMBEDDED_REPOSITORY);
 

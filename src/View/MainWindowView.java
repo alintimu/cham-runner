705cb0b5b32d3407938d86c122926a8e3677e7e8
axesdnd <axesdnd@gmail.com>
2014-08-08 16:41:24 +0300

    fuck this shit

--- src/View/MainWindowView.java
+++ src/View/MainWindowView.java
@@ -5,20 +5,21 @@ import Model.TextFieldModel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 /**
  * Created by alin.timu on 8/8/2014.
  */
 public class MainWindowView extends JFrame implements AbstractWindowView, ItemListener {
     public JCheckBox templates_rb, cp_rb, selfService_rb;
-    public JTextField temp_source_tf, temp_dest_tf, cp_tf, selfService_tf;
     private String VIEW_IDENTIFIER = "";
     private MainController controller;
     private TextFieldModel textFieldModel = new TextFieldModel();
     // Components
     private JPanel windowPanel, templatesPanel, cpPanel, selfServicePanel;
-    private JLabel sourceLabel, destinationLabel;
     private JButton startButton;
     private int width = 200, height = 50;
 
@@ -38,7 +39,7 @@ public class MainWindowView extends JFrame implements AbstractWindowView, ItemLi
         windowPanel.setLayout(new BoxLayout(windowPanel, BoxLayout.PAGE_AXIS));
 
         startButton = new JButton("Start");
-        startButton.setPreferredSize(new Dimension(100,20));
+        startButton.setPreferredSize(new Dimension(100, 20));
 
         startButton.addActionListener(new ActionListener() {
 
@@ -87,47 +88,17 @@ public class MainWindowView extends JFrame implements AbstractWindowView, ItemLi
         templatesPanel.setLayout(new BoxLayout(templatesPanel, BoxLayout.LINE_AXIS));
         templatesPanel.setBorder(BorderFactory.createTitledBorder("Templates WAR"));
 
-
-        JPanel sourcePanel = new JPanel();
         JPanel temp_rb_panel = new JPanel();
 
         templates_rb = new JCheckBox("templates.war");
 
         templates_rb.addItemListener(this);
 
-        sourceLabel = new JLabel("Source path:");
-        destinationLabel = new JLabel("Destination path:");
-
-        temp_source_tf = new JTextField();
-        temp_dest_tf = new JTextField();
-
         templates_rb.setSelected(false);
 
         temp_rb_panel.add(templates_rb);
 
-
-        // Creating and adding the text field.
-
-        temp_source_tf.setPreferredSize(new Dimension(300, 20));
-
-
-        sourcePanel.add(sourceLabel);
-        sourcePanel.add(temp_source_tf);
-
-
-        // Creating and adding the text field.
-
-        temp_dest_tf.setPreferredSize(new Dimension(300, 20));
-
-
-        JPanel destPanel = new JPanel();
-        destPanel.add(destinationLabel);
-        destPanel.add(temp_dest_tf);
-
-
         templatesPanel.add(temp_rb_panel);
-        templatesPanel.add(sourcePanel);
-        templatesPanel.add(destPanel);
 
         return templatesPanel;
     }
@@ -157,7 +128,7 @@ public class MainWindowView extends JFrame implements AbstractWindowView, ItemLi
         }
     }
 
-    public void getInput(){
+    public void getInput() {
         textFieldModel.setTemplateSourcePath(temp_source_tf.getText());
         textFieldModel.setTemplateDestinationPath(temp_dest_tf.getText());
         //temp_dest_tf.setText(textFieldModel.getTemplateSourcePath());
